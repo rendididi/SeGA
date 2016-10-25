af82769f7727d5cdfdab3252a1813174afb3450e
xingfeihappy <805915717@qq.com>
2016-10-25 16:48:41 +0800

    2016-10-25 16:48

--- TaskViewer/src/main/java/org/sega/viewer/config/JpaConfig.java
+++ TaskViewer/src/main/java/org/sega/viewer/config/JpaConfig.java
@@ -4,13 +4,14 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.sega.viewer.Application;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -18,50 +19,37 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import org.sega.viewer.Application;
+import org.sega.viewer.controllers.HomeController;
 
 @Configuration
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackageClasses = Application.class)
-abstract
-class JpaConfig implements TransactionManagementConfigurer {
+@EnableJpaRepositories(basePackageClasses = Application.class,entityManagerFactoryRef="entityManagerFactory1",transactionManagerRef = "mysql1TransactionManager")
+class JpaConfigMysql1 implements TransactionManagementConfigurer {
 
     @Value("${spring.dataSource.mysql1.driverClassName}")
-    private String driver1;
+    private String driver;
     @Value("${spring.dataSource.mysql1.url}")
-    private String url1;
+    private String url;
     @Value("${spring.dataSource.mysql1.username}")
-    private String username1;
+    private String username;
     @Value("${spring.dataSource.mysql1.password}")
-    private String password1;
-    
-    @Value("${spring.dataSource.mysql2.driverClassName}")
-    private String driver2;
-    @Value("${spring.dataSource.mysql2.url}")
-    private String url2;
-    @Value("${spring.dataSource.mysql2.username}")
-    private String username2;
-    @Value("${spring.dataSource.mysql2.password}")
-    private String password2;
-    
+    private String password;
     @Value("${spring.hibernate.dialect}")
     private String dialect;
     @Value("${spring.hibernate.hbm2ddl.auto}")
     private String hbm2ddlAuto;
     @Value("${spring.hibernate.show_sql}")
     private Boolean showSql;
-    private static final Logger logger = LoggerFactory.getLogger(JpaConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(JpaConfigMysql1.class);
     @Bean
-    @Primary
     public DataSource configureDataSource1() {
         HikariConfig config = new HikariConfig();
-        logger.debug("driver1====================:"+driver1);
-        config.setDriverClassName(driver1);
-        config.setJdbcUrl(url1);
-        config.setUsername(username1);
-        config.setPassword(password1);
+        logger.debug("driver driver driver driver driver driver :"+driver);
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
 
         config.addDataSourceProperty("useUnicode", "true");
         config.addDataSourceProperty("characterEncoding", "utf8");
@@ -72,7 +60,8 @@ class JpaConfig implements TransactionManagementConfigurer {
 
         return new HikariDataSource(config);
     }
-    @Bean(name = "entityManagerFactory1")
+
+    @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory1() {
         LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactoryBean.setDataSource(configureDataSource1());
@@ -87,12 +76,9 @@ class JpaConfig implements TransactionManagementConfigurer {
 
         return entityManagerFactoryBean;
     }
+
     @Bean(name = "mysql1TransactionManager")
-    @Primary
-    public PlatformTransactionManager annotationDrivenTransactionManager1() {
-    	JpaTransactionManager manager = new JpaTransactionManager();  
-        manager.setEntityManagerFactory(entityManagerFactory1().getObject());  
-        return manager;  
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new JpaTransactionManager();
     }
-    
 }
